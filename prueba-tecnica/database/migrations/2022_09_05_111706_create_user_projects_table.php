9cd447dc96545514ebcceb286531633b578858df
JaimeGM96 <jaimegallardo@correo.ugr.es>
2022-09-05 14:48:55 +0200

    Añadidas migraciones para las tablas de relaciones entre modelos (#4)

---
+++ prueba-tecnica/database/migrations/2022_09_05_111706_create_user_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_projects', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->integer('role_id');
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->unique(['user_id', 'project_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_projects');
+    }
+};
---
+++ prueba-tecnica/database/migrations/2022_09_05_112150_create_user_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->integer('role_id');
+            $table->foreignId('activity_id')->constrained()->cascadeOnDelete();
+            $table->unique(['user_id', 'activity_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};
